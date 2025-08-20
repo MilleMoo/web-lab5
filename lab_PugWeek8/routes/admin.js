9bd9fabc6e30f11b0727254e7e159b70976f7d27
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2025-08-20 14:53:41 +0700

    Add files via upload

---
+++ lab_PugWeek8/routes/admin.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+const products = [];
+
+router.get("/add-product", (req, res) => {
+  res.render("add-product", { pageTitle: "Add Product" });
+});
+
+router.post("/add-product", (req, res) => {
+  products.push({
+      title: req.body.title
+    });
+  console.log(products);
+  res.redirect("/");
+});
+
+exports.routes = router;
+exports.products = products;
---
+++ lab_PugWeek8/routes/shop.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  console.log("shop", products);
+  res.render("shop", { pageTitle: "My Shop Mak Mak", products: products });
+});
+
+module.exports = router;
